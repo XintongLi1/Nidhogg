e0930861e3ae191eee15a96767b793bd446f4160
XintongLi1 <1720731919@qq.com>
2021-05-07 00:08:24 -0400

    structured minimax

--- minimax.py
+++ minimax.py
@@ -19,6 +19,7 @@ Heuristics:
 created_lookup = False
 zobrist_lookup_table = []
 
+
 class Minimax:
     def __init__(self, board, me):  # board = data["board"], me = data["me"]
         global zobrist_lookup_table, created_lookup
@@ -38,7 +39,7 @@ class Minimax:
             zobrist_lookup_table = [[[0] + [random.randint(1, 2**64 - 1) for _ in range(124)] for _ in range(self.start.width)] for _ in range(self.start.height)]
             created_lookup = True
         self.zobrist_lookup = zobrist_lookup_table
-        self.states = [[] for _ in range(2**16)]
+        self.states = [[] for _ in range(2 ** 16)]
 
     def zobristHash(self, board=None):
         if board is None:
@@ -73,6 +74,14 @@ class Minimax:
                 return True
         return False
 
+    def find_snake(self, position_y, position_x):
+        pass
+
+    def move_snake(self, snake, direction, forward=True, state_board=None):
+        if state_board is None:
+            state_board = self.start.board
+        pass
+
     def update_state_hash_value(self, hash_value, snake, to_position_y, to_position_x, board=None):
         if board is None:
             board = self.start.board
@@ -94,9 +103,17 @@ class Minimax:
                         hash_value ^= self.zobrist_lookup[to_position_y][to_position_x][63 + snake["length"]]
         return hash_value
 
-    def is_dead_end(self, snake, state_board=None):
+    def is_dead_end(self, snake, to_position_y, to_position_x, state_board=None):
         if state_board is None:
             state_board = self.start.board
+        if state_board[to_position_y][to_position_x] == 1:  # body
+            return True
+        if state_board[to_position_y][to_position_x] == 2:  # rival's head
+            for any_snake in self.start.snakes:
+                if any_snake["head"]['y'] == to_position_y and any_snake["head"]['x'] == to_position_x \
+                        and any_snake["length"] >= snake["length"]:
+                    return True
+        return False
 
     def get_score(self, state_board=None):
         if state_board is None:
@@ -109,15 +126,55 @@ class Minimax:
         """
         if state_board is None:
             state_board = self.start.board
-        if depth == 0 :
+        if depth == 0:
             return self.get_score(state_board), None, None
-        if self.is_dead_end(state_board):
-            return -inf, None, None if player["id"] == self.start.me["id"] else inf, None, None
 
         if player["id"] == self.start.me["id"]:  # maximizing player
             best_score, best_direction, killed_rival = -inf, None, False
             for direction, ny, nx in self.start.get_neighbors(player["head"]['y'], player["head"]['x']):
-                pass
+                if not self.is_dead_end(player, ny, nx, state_board):
+                    new_hash_value = self.update_state_hash_value(state_hash_value, player, ny, nx, state_board)
+                    previous = state_board[ny][nx]
+                    has_food = (previous == 4)
+                    can_kill_rival = (previous == 2)
+                    self.move_snake(player, direction, True, state_board)
+                    if can_kill_rival:
+                        self.find_snake(ny, nx)
+                        # remove the snake
+                        pass
+
+                    score, kill = -inf, 0
+                    for snake in self.start.snakes:
+                        if snake["id"] != player["id"]:
+                            this_score, _, this_kill = self.minimax(new_hash_value, depth-1, snake, alpha, beta)
+                            if this_score > score or (this_score == score and this_kill > kill):
+                                score, kill = this_score, this_kill
+
+                    if score >= best_score and score > -inf:
+                        best_score = score
+                        best_direction = [direction] if score > best_score else best_direction + [direction]
+                    # revert the state_board
+                    self.move_snake(player, direction, False, state_board)
+                    if can_kill_rival:
+                        # restore the snake
+                        pass
+                    state_board[ny][nx] = previous
+
+                    can_kill_rival = can_kill_rival + kill
+                    if can_kill_rival:
+                        killed_rival = [(direction, can_kill_rival)] if not killed_rival else killed_rival + [(direction, can_kill_rival)]
+                    alpha = max(alpha, score)
+                    if alpha >= beta:
+                        break
+            if len(best_direction > 2) and killed_rival :
+                killed_rival.sort(key=lambda x: x[0])
+                for _, direction in killed_rival:
+                    if direction in best_direction:
+                        return best_score, direction, killed_rival[0]
+
+
+
+
         else:  # minimizing player
             best_score, best_direction, killed_rival = inf, None, False
             for direction, ny, nx in self.start.get_neighbors(player["head"]['y'], player["head"]['x']):
